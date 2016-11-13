1cbb877c15ff63197dec5a5ce3275599676a126f
stephen.hyde <support@instanetwork.ca>
2016-11-13 13:27:59 -0500

    - Progress on hashtags

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -7,8 +7,22 @@ import {Component, ViewEncapsulation} from '@angular/core';
   template: require('./service.html')
 })
 export class Service {
-
+  private hashtags: string[] = [];
+  private inputValue: string = "";
+  private inputValid: boolean = false;
   constructor() {
+    this.hashtags.push("Love");
+    this.hashtags.push("Smile");
+    this.hashtags.push("Laugh");
+    this.hashtags.push("Happy");
+    this.hashtags.push("Life");
   }
 
+  private addHashtag() {
+    this.hashtags.unshift(this.inputValue);
+  }
+  newItemChanged(event: KeyboardEvent): void {
+    const target = <HTMLInputElement> event.target;
+    this.inputValue = target.value;
+  }
 }
