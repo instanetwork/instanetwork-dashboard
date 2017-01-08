11ce4096e0206041558af92d7927bf7dbf9e6abb
stephen.hyde <support@instanetwork.ca>
2017-01-08 16:31:52 -0500

    - More code for change password page

--- src/app/pages/change_password/change.component.ts
+++ src/app/pages/change_password/change.component.ts
@@ -1,5 +1,7 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {AuthenticationService} from '../change_password/index';
+import {ReCaptchaComponent} from 'angular2-recaptcha/lib/captcha.component';
 
 @Component({
   selector: 'register',
@@ -9,28 +11,36 @@ import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/form
 })
 export class Change {
 
+  @ViewChild(ReCaptchaComponent) captcha:ReCaptchaComponent;
+
   public form:FormGroup;
   public password:AbstractControl;
   public repeatPassword:AbstractControl;
-
+  public passwords: FormGroup;
+  private error: string = '';
+  private loading: boolean = false;
   public submitted:boolean = false;
 
   constructor(fb:FormBuilder) {
 
     this.form = fb.group({
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      'passwords': fb.group({
+        'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+        'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      })
     });
 
-    this.password = this.form.controls['password'];
-    this.repeatPassword = this.form.controls['repeatPassword'];
+    this.passwords = <FormGroup> this.form.controls['passwords'];
+    this.password = this.passwords.controls['password'];
+    this.repeatPassword = this.passwords.controls['repeatPassword'];
   }
 
   public onSubmit(values:Object):void {
+    this.error = '';
+    this.loading = true;
     this.submitted = true;
     if (this.form.valid) {
-      // your code goes here
-      // console.log(values);
+       console.log(values);
     }
   }
 }
