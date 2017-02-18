914350084144d25eb77925ab78254c931ca7f978
Stephen Hyde <stephen.hyde@Stephens-MacBook-Pro.local>
2017-02-18 11:08:00 -0500

    - Stripe Progress

--- src/app/pages/subscription/subscription.component.ts
+++ src/app/pages/subscription/subscription.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewEncapsulation, Renderer} from '@angular/core';
 
 @Component({
   selector: 'subscription',
@@ -8,8 +8,45 @@ import {Component, ViewEncapsulation} from '@angular/core';
 })
 export class Subscription {
   private currentUser: number;
-  constructor() {
+  private globalListener: any;
+
+  constructor(private renderer: Renderer) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
+  private openCheckoutBusiness() {
+   this.checkout(1000, 'Business Package');
+  }
+
+  private openCheckoutPremium() {
+    this.checkout(8000, 'Premium Package');
+  }
+
+  private openCheckoutPrimary() {
+    this.checkout(6000, 'Primary Package');
+  }
+
+  private checkout(price, desc) {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_rgv209vKtNJJOruFdt12meDo',
+      locale: 'auto',
+      token: function (token: any) {
+        console.log("hey " + token.id);
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+      }
+    });
+
+    handler.open({
+      name: 'INSTANETWORK INC',
+      description: desc,
+      amount: price,
+      image: './assets/img/stripe_logo.jpg',
+      currency: 'cad'
+    });
+
+    this.globalListener = this.renderer.listenGlobal('window', 'popstate', () => {
+      handler.close();
+    });
+  }
 }
